aed5a29a13a147489f427d49bc281d8551b4ccac
ayaa & alaa <nahjanulahyar52@gmail.com>
2025-01-27 20:50:42 +0700

    Update banner.js

--- config/banner.js
+++ config/banner.js
@@ -2,7 +2,7 @@ import figlet from "figlet";
 import { colors } from "./colors.js";
 
 export function getBanner() {
-  const banner = figlet.textSync("LayerEdge Script", {
+  const banner = figlet.textSync("-----", {
     font: "ANSI Shadow",
     horizontalLayout: "default",
     verticalLayout: "default",
@@ -12,16 +12,7 @@ export function getBanner() {
   let output = [];
   output.push(`${colors.bannerText}${banner}${colors.reset}`);
   output.push(
-    `${colors.bannerBorder}===============================================${colors.reset}`
-  );
-  output.push(
-    `${colors.bannerLinks}GitHub  : https://github.com/Galkurta${colors.reset}`
-  );
-  output.push(
-    `${colors.bannerLinks}Telegram: https://t.me/galkurtarchive${colors.reset}`
-  );
-  output.push(
-    `${colors.bannerBorder}===============================================\n${colors.reset}`
+    `${colors.bannerBorder}===AYAA-ALAA===\n${colors.reset}`
   );
 
   return output.join("\n");
